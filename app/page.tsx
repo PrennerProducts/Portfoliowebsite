f96427beb9668e5409a16cdaad2e39a688155b9c
PrennerProducts <prenner.products@gmail.com>
2024-05-29 02:13:47 +0200

    update Hero

--- app/page.tsx
+++ app/page.tsx
@@ -15,8 +15,8 @@ const Home = () => {
   return (
     <>
       <Hero />
-      <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-        <div className="max-w-7xl w-full">
+      <main className="relative bg-black-100 flex flex-col overflow-hidden mx-auto w-full">
+        <div className="max-w-7xl w-full mx-auto px-5 sm:px-10">
           <FloatingNav navItems={navItems} />
           <Grid />
           <RecentProjects />
